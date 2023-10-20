10ff1a52a3b8499b537fbdbe77c9c1af730551fc
Ville Niemi <vipeni35@gmail.com>
2023-10-20 16:45:06 +0300

    about page text updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 
 
-//Theme Switcher
 const themeButton = document.getElementById("themeButton");
-if (themeButton !== null) {
-    themeButton.addEventListener("click", function() {
-        console.log("the button was clicked!");
+
+if (themeButton) {
+    themeButton.addEventListener("click", () => {
+        console.log("The button was clicked!");
         document.body.classList.toggle("dark-theme");
     });
-}else{
-    console.log("it's null");
+} else {
+    console.log("The themeButton element is null.");
 }
 
 // Scrolling Reveal Effect
@@ -22,34 +22,45 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const hiddenElements = document.querySelectorAll('.section-hidden')
+const hiddenElements = document.querySelectorAll('.section-hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 // Fetch and choose random quote
-const jsonFileUrl = 'data/quotes.json';
+const quotesUrl = 'data/quotes.json';
 const quoteText = document.getElementById('quote-text');
 
 async function getRandomQuote() {
-  try {
-    const response = await fetch(jsonFileUrl);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from ${jsonFileUrl}`);
-    }
-    const data = await response.json();
+    try {
+        const response = await fetch(quotesUrl);
 
-    const randomGenerator = Math.floor(Math.random() * data.length);
-    const randomQuote = data[randomGenerator];
-    quoteText.textContent = randomQuote;
-    
-  } catch (error) {
-    console.error(error);
-  }
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data from ${quotesUrl}`);
+        }
+
+        const data = await response.json();
+        const randomGenerator = Math.floor(Math.random() * data.length);
+        const randomQuote = data[randomGenerator];
+        quoteText.textContent = randomQuote;
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-// 10 second timer to update random quote
+// 10-second timer to update random quote
 function updateQuotePeriodically() {
     getRandomQuote();
-    setInterval(getRandomQuote, 10000); 
+    setInterval(getRandomQuote, 10000);
 }
-  
 updateQuotePeriodically();
+
+
+// Event listener for each aside button
+const buttons = document.querySelectorAll('.btn-aside');
+const sections = document.querySelectorAll('.section-hidden');
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        sections[index].scrollIntoView({ behavior: 'smooth' });
+    });
+});
