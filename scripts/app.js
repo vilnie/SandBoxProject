b1e3ee82e6d9b9aa6d1e324ba6b2deb616075068
Ville Niemi <vipeni35@gmail.com>
2023-10-20 18:10:36 +0300

    Adjustments to theme switcher
    Adjustments to theme switcher and small changes to responsiveness

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,87 @@
 
-
+// Theme Switcher
 const themeButton = document.getElementById("themeButton");
+const body = document.body;
+const darkThemeClass = "dark-theme";
+let isDarkTheme = false;
 
+// Check for a user's theme preference in local storage
+if (localStorage.getItem("themePreference") === "dark") {
+  isDarkTheme = true;
+  body.classList.add(darkThemeClass);
+}
+
+// Function to toggle the theme
+function toggleTheme() {
+  isDarkTheme = !isDarkTheme;
+  if (isDarkTheme) {
+    body.classList.add(darkThemeClass);
+    localStorage.setItem("themePreference", "dark");
+  } else {
+    body.classList.remove(darkThemeClass);
+    localStorage.setItem("themePreference", "light");
+  }
+}
+
+// Add an event listener to the theme button
 if (themeButton) {
-    themeButton.addEventListener("click", () => {
-        console.log("The button was clicked!");
-        document.body.classList.toggle("dark-theme");
-    });
+  themeButton.addEventListener("click", () => {
+    toggleTheme();
+    console.log("The theme button was clicked!");
+  });
 } else {
-    console.log("The themeButton element is null.");
+  console.log("The themeButton element is null.");
 }
 
 // Scrolling Reveal Effect
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('section-show');
-        } else {
-            entry.target.classList.remove('section-show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('section-show');
+    } else {
+      entry.target.classList.remove('section-show');
+    }
+  });
 });
 
 const hiddenElements = document.querySelectorAll('.section-hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// Fetch and choose random quote
+// Fetch and display a random quote
 const quotesUrl = 'data/quotes.json';
 const quoteText = document.getElementById('quote-text');
 
 async function getRandomQuote() {
-    try {
-        const response = await fetch(quotesUrl);
+  try {
+    const response = await fetch(quotesUrl);
 
-        if (!response.ok) {
-            throw new Error(`Failed to fetch data from ${quotesUrl}`);
-        }
-
-        const data = await response.json();
-        const randomGenerator = Math.floor(Math.random() * data.length);
-        const randomQuote = data[randomGenerator];
-        quoteText.textContent = randomQuote;
-
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from ${quotesUrl}`);
     }
+
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomQuote = data[randomIndex];
+    quoteText.textContent = randomQuote;
+
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-// 10-second timer to update random quote
+// Periodically update the random quote every 10 seconds
 function updateQuotePeriodically() {
-    getRandomQuote();
-    setInterval(getRandomQuote, 10000);
+  getRandomQuote();
+  setInterval(getRandomQuote, 10000);
 }
 updateQuotePeriodically();
 
-
-// Event listener for each aside button
+// Event listeners for aside buttons to scroll to sections
 const buttons = document.querySelectorAll('.btn-aside');
 const sections = document.querySelectorAll('.section-hidden');
 
 buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        sections[index].scrollIntoView({ behavior: 'smooth' });
-    });
+  button.addEventListener('click', () => {
+    sections[index].scrollIntoView({ behavior: 'smooth' });
+  });
 });
